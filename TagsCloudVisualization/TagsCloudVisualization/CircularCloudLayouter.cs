9c3ad8efdd6f1e283aba05adb63f6182488b1779
Yewert <blazer.andrew@gmail.com>
2017-11-04 19:34:19 +0500

    1

--- TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
+++ TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace TagsCloudVisualization
 {
     public class CircularCloudLayouter
     {
+        private const double deltaAngle = Math.PI / 16;
+        
         private readonly Point center;
 
         public int LeftBound
@@ -35,6 +39,10 @@ namespace TagsCloudVisualization
         private int rightBound = 0;
         private int upperBound = 0;
         private int lowerBound = 0;
+        
+        private readonly List<Rectangle> rectangles;
+
+        public int Count => rectangles.Count;
 
         public int Width => RightBound - LeftBound;
         public int Height => LowerBound - UpperBound;
@@ -42,16 +50,45 @@ namespace TagsCloudVisualization
         public CircularCloudLayouter(Point center)
         {
             this.center = center;
+            this.rectangles = new List<Rectangle>();
         }
 
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
-            var upperLeftCorner = new Point(center.X - rectangleSize.Width / 2, center.Y - rectangleSize.Height / 2);
-            LeftBound = upperLeftCorner.X;
-            RightBound = upperLeftCorner.X + rectangleSize.Width;
-            UpperBound = upperLeftCorner.Y;
-            LowerBound = upperLeftCorner.Y + rectangleSize.Height;
-            return new Rectangle(upperLeftCorner, rectangleSize);
+            var rectangle = PlaceNewRectangle(rectangleSize);
+            LeftBound = rectangle.X;
+            RightBound = rectangle.X + rectangleSize.Width;
+            UpperBound = rectangle.Y;
+            LowerBound = rectangle.Y + rectangleSize.Height;
+            rectangles.Add(rectangle);
+            return rectangle;
+        }
+
+        private Rectangle PlaceNewRectangle(Size rectangleSize)
+        {
+            var i = 0;
+            while (true)
+            {
+                var shiftFromCenter = TransformCoordinatesFromPolarToCartesian(i * deltaAngle, i * deltaAngle);
+                var upperLeftCorner = new Point(center.X + shiftFromCenter.X - rectangleSize.Width / 2,
+                    center.Y + shiftFromCenter.Y - rectangleSize.Height / 2);
+                var rectangle = new Rectangle(upperLeftCorner, rectangleSize);
+                if (!IntersectsWithAnyOtherRectangle(rectangle))
+                    return rectangle;
+                i++;
+            }
+        }
+
+        public bool IntersectsWithAnyOtherRectangle(Rectangle rect)
+        {
+            return rectangles.Any(r => r.IntersectsWith(rect));
+        }
+
+        private (int X, int Y) TransformCoordinatesFromPolarToCartesian(double angle, double length)
+        {
+            var x = (int) (length * Math.Cos(angle));
+            var y = (int) (length * Math.Sin(angle));
+            return (x, y);
         }
     }
 }
--- TagsCloudVisualization/TagsCloudVisualization/TestCircularCloudLayouter.cs
+++ TagsCloudVisualization/TagsCloudVisualization/TestCircularCloudLayouter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using FluentAssertions;
 using NUnit.Framework;
@@ -35,6 +36,55 @@ namespace TagsCloudVisualization
                 layouter.UpperBound, layouter.LowerBound,
                 layouter.Width, layouter.Height);
             actualDimensions.ShouldBeEquivalentTo((-5, 5, -5, 5, 10, 10));
+        }
+
+        [Test]
+        public void HasCorrectCount_WhenMultipleRectanglesAdded()
+        {
+            var center = new Point(0, 0);
+            var size = new Size(10, 10);
+            var layouter = new CircularCloudLayouter(center);
+            layouter.PutNextRectangle(size);
+            layouter.PutNextRectangle(size);
+            Assert.AreEqual(2, layouter.Count);
+        }
+
+        [Test]
+        public void RectanglesDoNotIntersect_WhenPutTwoRectangles()
+        {
+            var center = new Point(0, 0);
+            var size = new Size(10, 10);
+            var layouter = new CircularCloudLayouter(center);
+            var rect1 = layouter.PutNextRectangle(size);
+            var rect2 = layouter.PutNextRectangle(size);
+            Assert.False(rect1.IntersectsWith(rect2));
+        }
+
+        
+        //TODO: ЭТО ГОВНОКОД ДЛЯ ВИЗУАЛЬНОЙ ОЦЕНКИ, В ФИНАЛЬНОЙ ВЕРСИИ ВЫПИЛИТЬ
+        [Test]
+        public void l()
+        {
+            var center = new Point(0, 0);
+            var size = new Size(1, 1);
+            var layouter = new CircularCloudLayouter(center);
+            var rects = new List<Rectangle>();
+            for (int i = 0; i < 100; i++)
+            {
+                var s = new Size((int)(size.Width + i * 0.5), (int)(size.Height + i * 0.25));
+                rects.Add(layouter.PutNextRectangle(s));
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                rects[i] = new Rectangle(rects[i].X + center.X- layouter.LeftBound + 10,
+                    rects[i].Y + center.Y - layouter.UpperBound + 10, rects[i].Width, rects[i].Height);
+            }
+            var image = new Bitmap(layouter.Width + 20, layouter.Height + 20);
+            var graphics = Graphics.FromImage(image);
+            graphics.DrawRectangles(Pens.Black, rects.ToArray());
+            graphics.DrawEllipse(Pens.Blue, center.X + center.X - layouter.LeftBound + 10, center.Y + center.Y - layouter.UpperBound + 10, 1, 1);
+            image.Save("C:/Users/Денис/Documents/C#/tdd/TagsCloudVisualization/cool3.png", ImageFormat.Png);
+            Assert.True(true);
         }
     }
 }
