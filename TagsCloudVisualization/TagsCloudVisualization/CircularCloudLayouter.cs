83230b6185d1e748a259289d934162c778d5a283
Yewert <blazer.andrew@gmail.com>
2017-11-04 15:08:25 +0500

    1

---
+++ TagsCloudVisualization/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        private readonly Point center;
+
+        public int LeftBound
+        {
+            get => leftBound;
+            private set => leftBound = Math.Min(leftBound, value);
+        }
+
+        public int RightBound
+        {
+            get => rightBound;
+            private set => rightBound = Math.Max(rightBound, value);
+        }
+
+        public int UpperBound
+        {
+            get => upperBound;
+            private set => upperBound = Math.Min(upperBound, value);
+        }
+
+        public int LowerBound
+        {
+            get => lowerBound;
+            private set => lowerBound = Math.Max(lowerBound, value);
+        }
+
+        private int leftBound = 0;
+        private int rightBound = 0;
+        private int upperBound = 0;
+        private int lowerBound = 0;
+
+        public int Width => RightBound - LeftBound;
+        public int Height => LowerBound - UpperBound;
+        
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            var upperLeftCorner = new Point(center.X - rectangleSize.Width / 2, center.Y - rectangleSize.Height / 2);
+            LeftBound = upperLeftCorner.X;
+            RightBound = upperLeftCorner.X + rectangleSize.Width;
+            UpperBound = upperLeftCorner.Y;
+            LowerBound = upperLeftCorner.Y + rectangleSize.Height;
+            return new Rectangle(upperLeftCorner, rectangleSize);
+        }
+    }
+}
---
+++ TagsCloudVisualization/TagsCloudVisualization/TestCircularCloudLayouter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class TestCircularCloudLayouter
+    {
+        [TestCase(0, 0, 10, 10, -5, -5)]
+        [TestCase(10, 10, 10, 10, 5, 5)]
+        [TestCase(10, 10, 11, 11, 5, 5)]
+        public void PutsInCenter_WhenFirstRectangleGiven(
+            int centerX, int centerY, int rectangleWidth, int rectangleHeight, int rectangleX, int rectangleY)
+        {
+            var center = new Point(centerX, centerY);
+            var size = new Size(rectangleWidth, rectangleHeight);
+            var rectangleCoords = new Point(rectangleX, rectangleY);
+            var rectangle = new Rectangle(rectangleCoords, size);
+            var layouter = new CircularCloudLayouter(center);
+            layouter.PutNextRectangle(size).ShouldBeEquivalentTo(rectangle);
+        }
+
+        [Test]
+        public void HasCorrectBounds_AfterPuttingFirstRectangle()
+        {
+            var center = new Point(0, 0);
+            var size = new Size(10, 10);
+            var layouter = new CircularCloudLayouter(center);
+            layouter.PutNextRectangle(size);
+            (int left, int right, int top, int bottom, int width, int height) actualDimensions =
+                (layouter.LeftBound, layouter.RightBound,
+                layouter.UpperBound, layouter.LowerBound,
+                layouter.Width, layouter.Height);
+            actualDimensions.ShouldBeEquivalentTo((-5, 5, -5, 5, 10, 10));
+        }
+    }
+}
